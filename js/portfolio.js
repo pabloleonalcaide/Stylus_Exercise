63c9c29ee55118e7a5564622993ca59d823071fe
Pablo Leon <pabloleonpsico@gmail.com>
2018-07-19 13:56:25 +0200

    Cleaning code
    Just cleaning the code

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,6 @@
 $(document).ready(main);
  
-var contador = true;
+var counter = true;
  
 function main(){
 	$('#projects a').mouseover(function(event){
@@ -12,45 +12,35 @@ function main(){
 	$('#menuLink').click(function(event){
 		event.preventDefault();
           $(this).hide().show("slide", { direction: "left" }, 1000);
-		if(contador){
-			mostrarNav();
+		if(counter){
+			showNav();
 		} else {
-			ocultarNav();
+			hideNav();
 		}
- 	//oculta el nav si hace click en el resto de la página, en el icono del menu desplegable o si pulsa la tecla de escape
- 	$('.navLink').click(ocultarNav) 
- 	$('main').click(ocultarNav)
+ 	$('.navLink').click(hideNav) 
+ 	$('main').click(hideNav)
  	$(document).keypress(function(e){
  		if(e.keyCode === 27)
- 			ocultarNav();
+ 			hideNav();
  	});
-	});
-/**
-* oculta la barra de navegación
-*/
- let ocultarNav =()=>{
- 	contador = true;
-		$('nav').animate({
-			left: '-100%'
-		});
+});
+	
+ let hideNav =()=>{
+ 	counter = true;
+		$('nav').animate({ left: '-100%' });
 		$('.underline').css({
             transition : 'color 1s ease-in-out',
                          "color": "#000"
            });
-
  }
- /**
- * muestra la barra de navegación
- */
- let mostrarNav = ()=>{
- 	$('nav').animate({
-		left: '0'
-	});
+ 
+ let showNav = ()=>{
+ 	$('nav').animate({ left: '0' });
 	$('.underline').css({
                 transition : 'color 1s ease-in-out',
                 "color": "#1BB1F5"
             });
-	contador = false;
+	counter = false;
  }
 
 };
